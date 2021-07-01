7cd09a9bc415a77cb746c2521815b731bd9aa37e
Akila SenthilKumar <akila.senthil2009@gmail.com>
2021-07-01 13:15:16 +0530

    rectified overflow

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,36 @@ import 'package:flutter/cupertino.dart';
 //void main() {
 //  runApp(MyApp());
 //}
-void main() => runApp(MyApp()); //----- same as the step above
+void main() => runApp(MyApp()); //same as the step above
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('SERENDIPITY'),
-        ),
-        body: Card(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: Text('SERENDIPITY'),
+          ),
+          body: SingleChildScrollView(
+            //wrapping with another widget to eliminate overflow
             child: Column(
-          children: <Widget>[
-            Image.asset('assets/img1.jpg'),
-            Text('Effort Makes you'),
-          ],
-        )),
-      ),
+              children: [
+                RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Image'),
+                ),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/img1.jpg'),
+                      Text('Effort Makes you'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
